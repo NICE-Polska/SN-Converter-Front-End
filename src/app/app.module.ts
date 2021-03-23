322ede48c2269101959ac8641a22aca24e906e8f
Piotr Głowacki <pglow@NICEPL.LOCAL>
2021-03-23 22:18:50 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    SearchComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
